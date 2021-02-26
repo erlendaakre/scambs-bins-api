8cd13cbdfdc894019a00dc9f75cd0de44cbfc51b
Erlend Aakre <erlend@aakre.io>
2021-02-26 16:38:34 +0000

    reworked some stuff

--- src/main/scala/io/aakre/scambsBinsApi/Server.scala
+++ src/main/scala/io/aakre/scambsBinsApi/Server.scala
@@ -2,16 +2,18 @@ package io.aakre.scambsBinsApi
 
 import cats.data.Kleisli
 import cats.effect._
-import cats.implicits._
 import org.http4s.server.blaze._
 import org.http4s.implicits._
 import org.http4s._
 import org.http4s.dsl.io._
 import org.http4s.server.Router
 import org.http4s.client.blaze._
+import io.circe._
+import io.circe.generic.auto._
+import io.circe.syntax._
+import org.http4s.client.Client
 
-import io.circe._, io.circe.generic.auto._, io.circe.syntax._
-
+import scala.concurrent.ExecutionContext
 import scala.concurrent.ExecutionContext.global
 
 object Server extends IOApp {
@@ -25,23 +27,26 @@ object Server extends IOApp {
   val binService: HttpRoutes[IO] = HttpRoutes.of[IO] {
     case GET -> Root / "bins" =>
       BlazeClientBuilder[IO](global).resource.use { client =>
-        val testProg = for {
-          rawIcal <- Network.readFromUrl(scambsIcalUrl, client)
-          parsed <- IO(ICalParsers.parse(ICalParsers.iCalParser, rawIcal))
-          prepared <- IO( Logic.joinAndSort(parsed.get))
-        } yield prepared.asJson.spaces2
-
-        Ok(testProg.unsafeRunSync())
+        Ok(binProg(client).unsafeRunSync())
       }
   }
 
   val binsApp: Action = Router("/" -> binService).orNotFound
 
-  def run(args: List[String]): IO[ExitCode] =
+  def binProg(client: Client[IO]): IO[String] = for {
+    rawIcal <- Network.readFromUrl(scambsIcalUrl, client)
+    parsed <- IO(ICalParsers.parse(ICalParsers.iCalParser, rawIcal))
+    prepared <- IO( Logic.joinAndSort(parsed.get))
+  } yield prepared.asJson.spaces2
+
+  def run(args: List[String]): IO[ExitCode] = {
+   // val ec: ExecutionContext = ExecutionContext.global
+
     BlazeServerBuilder[IO]
       .bindHttp(1701, "localhost")
       .withHttpApp(binsApp)
       .resource
       .use(_ => IO.never)
       .as(ExitCode.Success)
+  }
 }
