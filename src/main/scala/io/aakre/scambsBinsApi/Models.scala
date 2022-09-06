5a01838834159e7d1c3f937fd22a05bc8edfab79
Erlend Aakre <erlend@aakre.io>
2022-09-06 10:08:48 +0100

    model update

--- src/main/scala/io/aakre/scambsBinsApi/Models.scala
+++ src/main/scala/io/aakre/scambsBinsApi/Models.scala
@@ -9,19 +9,15 @@ final case class Date(year: Int, month: Int, day: Int) extends Ordered[Date] {
   override def compare(that: Date): Int = {
     Ordering.Tuple3(Ordering.Int, Ordering.Int, Ordering.Int).compare((year, month, day), (that.year, that.month, that.day))
   }
+
+  def isTomorrow: Boolean = {
+    // TODO
+    true
+  }
 }
 
 final case class Collection(date: Date, bins: List[Bin]) { self =>
-  /**
-   * Adds the bins of the given collection to this one
-   *
-   * @param that the second collection to copy the bins from
-   * @return a new collection with combined bins
-   */
-  def ++(that: Collection): Collection = {
-    Collection(date, self.bins ++ that.bins)
-  }
-
+  def ++(that: Collection): Collection = Collection(date, self.bins ++ that.bins)
   def +(bin: Bin): Collection = this.copy(bins = bin :: bins)
 }
 
